763c5217aa3ad9b4bef0d58dd6c81d895ecc51d4
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-09-30 18:44:11 -0300

    removed contato and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
